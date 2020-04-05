66d93b5f6fad0e694b3fecda497f41cbc951ed48
wakkihaya <wakkihaya@gmail.com>
2020-04-05 18:23:13 +0900

    firestore 連携

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+    <h1 @click="testFire">a</h1>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
@@ -33,10 +34,16 @@
 
 <script lang="ts">
 import { Component, Prop, Vue } from 'vue-property-decorator';
+import {db} from '../firebase';
 
 @Component
 export default class HelloWorld extends Vue {
   @Prop() private msg!: string;
+  public testFire(){
+    db.collection("articles").get()
+            .then(doc =>console.log(doc))
+  }
+
 }
 </script>
 
