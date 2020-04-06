eefcb6288ae6cdca450b1d2c18c854bc441e31b0
wakkihaya <wakkihaya@gmail.com>
2020-04-06 18:05:13 +0900

    user コレクションからarticle を拾ってくる

--- script/twitter_get.ts
+++ script/twitter_get.ts
@@ -5,7 +5,7 @@ import set = Reflect.set;
 var Twitter = require("twitter");
 var client = new Twitter({consumer_key,consumer_secret,access_token_key,access_token_secret});
 
-const twitterApiGet = function (url: string, params: any) {
+const twitterApiGet = function (url: string, params: any, user_id: string) {
     return new Promise((resolve,reject)=>{
         setTimeout(()=>{
         client.get(url,params,(error:any,response:any)=>{
@@ -18,8 +18,9 @@ const twitterApiGet = function (url: string, params: any) {
                 rt: null,
                 fav: null,
                 createdAt: "",
-                user_id:""
+                user_id: user_id
             };
+            var new_since_id: string = "";
             response.forEach(async(tweetItem: any) =>{
                 //console.log(tweetItem["text"]);
                 //console.log(tweetItem["entities"]);
@@ -30,11 +31,18 @@ const twitterApiGet = function (url: string, params: any) {
                     array.comment = tweetItem["text"];
                     array.rt = tweetItem["retweet_count"];
                     array.fav = tweetItem["favorite_count"];
+                    array.createdAt = tweetItem["created_at"];
                     console.log(array);
+                    new_since_id = tweetItem["id"];
+
                     //一つ一つのarray をDBに入れていく
-                    await addDataToDb(array);
+                   // await addDataToDb(array);
                 }
             });
+            //since_id を最新のものに変更
+            var updated_doc = {"since_id": new_since_id };
+            db.collection("users").doc(user_id).update(updated_doc);
+
             resolve();
         });
         },2000);
@@ -49,20 +57,24 @@ const addDataToDb = function (data: any) {
     })
 };
 
-
-var url: string = 'statuses/user_timeline';
-var params: any = {screen_name: 'suadd',include_rts: true, exclude_replies: true, since_id:"1246277346417504256"};
-
-
 //article のimage とtitle をupdate する関数
 
 
 (async ()=>{
     try {
-        const array = await twitterApiGet(url, params);
-        console.log("owari")
-       // await RequiredInfoGet(doc);
-        // console.log(doc)
+        const url: string = 'statuses/user_timeline';
+
+        db.collection("users").get()
+            .then(async(snapshot) =>{
+                for await(let doc of snapshot.docs){
+                    var s_name: string = doc.data().screen_name;
+                    var since_id: string = doc.data().since_id;
+                    var user_id: string = doc.id;
+                    console.log(user_id);
+                    var params: any = {screen_name: s_name,include_rts: true, exclude_replies: true, since_id: since_id}; //suadd のsince_id: 1246277346417504256
+                    await twitterApiGet(url,params,user_id);
+                }
+            });
     }catch (e) {
         console.log(e)
     }
