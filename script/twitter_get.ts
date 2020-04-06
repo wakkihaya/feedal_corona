9c48017dbe551376c4ad2b60bd81c981c1eaf208
wakkihaya <wakkihaya@gmail.com>
2020-04-06 17:39:16 +0900

    firebaseに保存

--- script/twitter_get.ts
+++ script/twitter_get.ts
@@ -1,50 +1,67 @@
 import {consumer_key,consumer_secret,access_token_key,access_token_secret} from './twitter_config'
+import {db} from '../src/firebase'
+import set = Reflect.set;
 
 var Twitter = require("twitter");
 var client = new Twitter({consumer_key,consumer_secret,access_token_key,access_token_secret});
 
 const twitterApiGet = function (url: string, params: any) {
     return new Promise((resolve,reject)=>{
+        setTimeout(()=>{
         client.get(url,params,(error:any,response:any)=>{
-            if(!error){
-                resolve(response);
-            }else{
-                reject(error);
-            }
-        })
+            var array: any =
+                {
+                url: "",
+                image: "",
+                title: "",
+                comment: "",
+                rt: null,
+                fav: null,
+                createdAt: "",
+                user_id:""
+            };
+            response.forEach(async(tweetItem: any) =>{
+                //console.log(tweetItem["text"]);
+                //console.log(tweetItem["entities"]);
+
+                var entities = tweetItem["entities"];
+                if(entities["urls"].length != 0){ //entities のurls が空でなかったら、シェアしたlinkあり
+                    array.url = entities["urls"][0].expanded_url;
+                    array.comment = tweetItem["text"];
+                    array.rt = tweetItem["retweet_count"];
+                    array.fav = tweetItem["favorite_count"];
+                    console.log(array);
+                    //一つ一つのarray をDBに入れていく
+                    await addDataToDb(array);
+                }
+            });
+            resolve();
+        });
+        },2000);
     })
 };
+
+//data をfirestore に入れる
+const addDataToDb = function (data: any) {
+    return new Promise((resolve, reject)=>{
+        db.collection("articles").add(data);
+        resolve();
+    })
+};
+
 
 var url: string = 'statuses/user_timeline';
 var params: any = {screen_name: 'suadd',include_rts: true, exclude_replies: true, since_id:"1246277346417504256"};
 
-const RequiredInfoGet = function (doc: any) {
-    return new Promise((resolve,reject)=>{
-        var array: any = {
-            url: "",
-            comment: "",
-            rt: null,
-            fav: null,
-        };
-        doc.forEach((tweetItem: any) =>{
-             //console.log(tweetItem["text"]);
-            //console.log(tweetItem["entities"]);
 
-            var entities = tweetItem["entities"];
-             if(entities["urls"].length != 0){             //entities のurls が空でなかったら、シェアしたlinkあり
-                 console.log(tweetItem["entities"]);
-                 array.url = entities["urls"][0].expanded_url;
-                 console.log(array.url);
-             }
+//article のimage とtitle をupdate する関数
 
-        })
-    })
-};
 
 (async ()=>{
     try {
-        const doc = await twitterApiGet(url, params);
-        await RequiredInfoGet(doc);
+        const array = await twitterApiGet(url, params);
+        console.log("owari")
+       // await RequiredInfoGet(doc);
         // console.log(doc)
     }catch (e) {
         console.log(e)
