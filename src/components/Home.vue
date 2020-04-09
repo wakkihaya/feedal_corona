cab8b1e72cae2b509e3441a6be17d9da83e0e496
wakkihaya <wakkihaya@gmail.com>
2020-04-09 20:45:19 +0900

    scss

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -26,7 +26,7 @@
 </template>
 
 <script lang="ts">
-import { Component, Prop, Vue } from 'vue-property-decorator';
+import { Component,  Vue } from 'vue-property-decorator';
 import {db} from '../firebase';
 
 @Component
